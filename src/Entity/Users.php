375e5137b4824be998f7ab1bfdf1c8f2b1dca4c4
kouassi <kouassi.duchaine@gmail.com>
2020-11-14 01:33:09 +0100

    |Add-Mod| - add_/Users( password,roles ), UsersRepo( method upPassw )/ mod_/Users(BDD), security.yaml  / -

--- src/Entity/Users.php
+++ src/Entity/Users.php
@@ -3,14 +3,15 @@
 namespace App\Entity;
 
 use App\Repository\UsersRepository;
+use Doctrine\ORM\Mapping as ORM;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
-use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass=UsersRepository::class)
  */
-class Users
+class Users implements UserInterface
 {
     /**
      * @ORM\Id
@@ -30,11 +31,27 @@ class Users
     private $firstname;
 
     /**
-     * @ORM\Column(type="string", length=50)
+     * @ORM\Column(type="date")
+     */
+    private $birth_date;
+
+    /**
+     * @ORM\Column(type="string", length=180, unique=true)
      */
     private $email;
 
     /**
+     * @ORM\Column(type="json")
+     */
+    private $roles = [];
+
+    /**
+     * @var string The hashed password
+     * @ORM\Column(type="string")
+     */
+    private $password;
+
+        /**
      * @ORM\Column(type="string", length=30)
      */
     private $city;
@@ -58,11 +75,6 @@ class Users
      * @ORM\OneToMany(targetEntity=Accounts::class, mappedBy="User")
      */
     private $accounts;
-
-    /**
-     * @ORM\Column(type="date")
-     */
-    private $birth_date;
 
     public function __construct()
     {
@@ -94,6 +106,18 @@ class Users
     public function setFirstname(string $firstname): self
     {
         $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    public function getBirthDate(): ?\DateTimeInterface
+    {
+        return $this->birth_date;
+    }
+
+    public function setBirthDate(\DateTimeInterface $birth_date): self
+    {
+        $this->birth_date = $birth_date;
 
         return $this;
     }
@@ -159,6 +183,67 @@ class Users
     }
 
     /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUsername(): string
+    {
+        return (string) $this->email;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getPassword(): string
+    {
+        return (string) $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getSalt()
+    {
+        // not needed when using the "bcrypt" algorithm in security.yaml
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+
+        /**
      * @return Collection|Accounts[]
      */
     public function getAccounts(): Collection
@@ -184,18 +269,6 @@ class Users
                 $account->setUser(null);
             }
         }
-
-        return $this;
-    }
-
-    public function getBirthDate(): ?\DateTimeInterface
-    {
-        return $this->birth_date;
-    }
-
-    public function setBirthDate(\DateTimeInterface $birth_date): self
-    {
-        $this->birth_date = $birth_date;
 
         return $this;
     }
