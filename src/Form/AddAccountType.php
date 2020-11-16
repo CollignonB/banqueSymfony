b143692b7993e9ac18a7eec172f47ebb4b46a913
Baptiste COLLIGNON <collignon@CDI>
2020-11-16 14:19:03 +0100

    add account form done

---
+++ src/Form/AddAccountType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Accounts;
+use App\Entity\AccountTypes;
+use Doctrine\ORM\EntityRepository;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+
+class AddAccountType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        // EntityType 
+        $builder
+            ->add('AccountType',EntityType::class, [
+                'class' => AccountTypes::class,
+                'query_builder' => function (EntityRepository $er) 
+                {
+                    return $er->createQueryBuilder('at')
+                        ->orderBy('at.name', 'ASC');
+                },
+                'choice_label' => 'name'
+            ])
+            ->add('amount')
+            ->add('save', SubmitType::class, [
+                'label' => 'Enregistrer le compte'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Accounts::class,
+        ]);
+    }
+}
